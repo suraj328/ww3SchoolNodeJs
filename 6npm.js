f41c375b2a48515964b1f95a5faa17421d52c45f
suraj328 <shahsuraj328@gmail.com>
2023-09-29 20:11:27 +0545

    sample

--- 6npm.js
+++
@@ -1,8 +0,0 @@
-const uc =  require('upper-case');
-const http =  require('http');
-
-http.createServer((req,res)=>{
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write(uc.upperCase('i am capital'));
-    res.end();
-}).listen(8080);
